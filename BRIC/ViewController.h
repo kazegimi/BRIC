f21287ef76c8baa374c4505457f89efb3aef25d7
cdp53190 <cdp53190@gmail.com>
2018-09-12 14:07:53 +0900

    Initial Commit

---
+++ BRIC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BRIC
+//
+//  Created by Eiichi Hayashi on 2018/09/12.
+//  Copyright © 2018年 skyElements. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
